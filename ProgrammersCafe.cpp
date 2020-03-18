5866109e5fcb8a0fea6b0a3148cff8bf4c266d10
Wafaa Wardah <wafaawardah89@gmail.com>
2020-03-18 21:49:05 +1200

    Added source file for cout practice

---
+++ ProgrammersCafe.cpp
@@ -0,0 +1,29 @@
+// Caffe Menu program, by Wafaa Wardah
+
+/*	Write a simple program that displays the menu of a cafe.
+	This can later be extended to a functional order and sale
+	program.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\n\t\tPROGRAMMERS\' CAFE\n\t\t-----------------\n\n";
+	cout << "\t  Locally owned and operated coffee house.\n"
+			"\t  Finest beverage selection in the country.\n"
+			"\t  Beverages made from the finest ingredients.\n\n";
+	
+	cout << "\t----------------------------------------------------\n"
+			"\t| Beverage    | Regular   | Large     | Extra Large|\n"
+			"\t----------------------------------------------------\n"
+			"\t| Long Black  | $3.50     | $3.70     | $4.00      |\n"
+			"\t| Flat White  | $3.80     | $4.20     | $4.50      |\n"
+			"\t| Cappuccino  | $4.00     | $4.30     | $5.00      |\n"
+			"\t| Tea         | $3.00     | $3.20     | $3.50      |\n"
+			"\t----------------------------------------------------\n\n\n";
+	
+	return 0;
+}
